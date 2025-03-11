29e2790a18884bd6d5a305febc898542604f264c
eleve <eleve@core>
2025-03-11 08:37:45 +0100

    fix

--- lib/vue/composant/appbar.dart
+++ lib/vue/composant/appbar.dart
@@ -1,1 +1,27 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Size get preferredSize => Size.fromHeight(60); // Hauteur de l'AppBar
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.purple,
+      centerTitle: true,
+      title: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/logo-forum.png', height: 40),
+          const SizedBox(width: 5),
+          Text(
+            "Forum",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
